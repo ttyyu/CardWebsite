346062e465d7f28ce5ddcbf43bd7f2f795ed6b4a
Tyrell Yu <tyrellyu@gmail.com>
2024-03-04 22:58:30 +0000

    Account Controller Added

--- obj/Debug/net8.0/CardWebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/CardWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68e458fefcea83840f6aca294cf2b9c80196f600")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3785482da8d79446dc64740bc8206198c338683")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
